f180dc31e75aa58886b34d038af23407147ec7d7
tantrifadhillah <90770432+tantrifadh@users.noreply.github.com>
2022-09-09 00:08:00 +0700

    Challenge 3

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,20 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+
+  for(let i = 0; i<cars.length; i++) {
+    if(cars[i].available) {
+      result.push(cars[i]);
+    }
+  }
+
+  console.log('Data sesudah filter car by available : ');
+  console.table(result);
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,23 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  for (let i=0; i < result.length; i++) {
+    for (let j=0; j < result.length - i - 1; j++)
+    if (result[j].year < result[j+1].year){
+      let x = result [j];
+      result[j] = result[j+1];
+      result[j+1] = x;
+    }
+  }
+
+  console.log('Data mobil sesudah Descending : ');
+  console.table(result);
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
